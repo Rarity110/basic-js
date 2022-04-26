4acae84a54341fe2f1f08fb15c3c7d36ecfd3f72
Rarity110 <rarity110@mail.ru>
2022-04-27 02:03:03 +0300

    refactor: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+
+
+function createDreamTeam(members) {
+  if (Array.isArray(members) === false) return false;    
+  let name = '';
+  let litters = [];
+  members.forEach(element => {
+    if (typeof element === "string" ) {
+      litters.push(element.trim()[0].toUpperCase());
+    }
+  })
+  litters.sort().forEach(element => {
+  name += element;
+  })
+  // console.log(name);
+  return name;
+};
+// createDreamTeam(['Peter', 'Adam', 'Gary', 'Bobby', 'Philip', 'Andrea', 'Evelyn', 'Evelyn', 'Kimberly', 'Denise'])
+
 
 module.exports = {
   createDreamTeam
