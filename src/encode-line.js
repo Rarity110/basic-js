e453788ec118a08508e6e25a9ab4c06e2e4c0e45
Rarity110 <rarity110@mail.ru>
2022-04-30 01:25:00 +0300

    refactor: encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str === '') return '';
+  let res = '';
+  let prevLitter = '';
+  let count = 1;
+  str.split('').forEach(litter => {
+    if (litter === prevLitter) {
+      count = count + 1;
+    } else if (count === 1) {
+        res = res + prevLitter; 
+        prevLitter = litter;       
+    } else {
+        res = res + count + prevLitter;
+        prevLitter = litter;    
+        count = 1; 
+    }
+  });
+  if (count === 1) {
+    res = res + str.split('')[str.split('').length - 1]; 
+  } else {
+    res = res + count + str.split('')[str.split('').length - 1]; 
 }
+return res;
+}
+
 
 module.exports = {
   encodeLine
