c56669c1a3956d8ec4eb8128ebb45aa5d0cb41fa
Rarity110 <rarity110@mail.ru>
2022-05-01 22:53:47 +0300

    carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (dateSample.arguments.length === 0 || typeof sampleActivity !== 'string') return false;
+  let number = parseFloat(sampleActivity);
+  if (!number || number < 0 || number > MODERN_ACTIVITY) return false;
+  let res = Math.ceil(Math.log(MODERN_ACTIVITY / number) / (0.693 / HALF_LIFE_PERIOD));
+  // console.log(res);
+  return res;
 }
+
 
 module.exports = {
   dateSample
