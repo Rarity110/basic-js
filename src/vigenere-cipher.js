c81fcd61bf875f59146b96470d9c388d752f4cce
Rarity110 <rarity110@mail.ru>
2022-05-01 22:24:46 +0300

    vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,108 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type) {
+    if (type === false) {
+      this.direction = false;
+    } else {
+      this.direction = true;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  encrypt(message, key) {
+    if (!key || !message) {
+      throw new Error('Incorrect arguments!');
+    }
+    let newKey = '';
+    let numOfKey = Math.floor(message.length / key.length);
+    for (let i = 0; i < numOfKey; i++) {
+      newKey = newKey + key;
+    }
+    for (let j = 0; j < (message.length % key.length); j++) {
+      newKey = newKey + key[j]
+    }
+    console.log(newKey);  
+    let res = '';
+    let alphabeth = ['A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let rows = alphabeth.length;
+    console.log((rows));
+    let massMessage = message.toUpperCase().split('');
+    let massRes = [];
+    let count = 0;
+    for (let x = 0; x < massMessage.length; x++) {
+      if (alphabeth.includes(massMessage[x])) {
+        let indA = alphabeth.indexOf(massMessage[x].toUpperCase());
+        let indB = alphabeth.indexOf(newKey[count].toUpperCase());
+        massRes.push(alphabeth[(indA + indB) % 26]);
+        count ++;
+      } else {
+        massRes.push(massMessage[x]);
+      }
+    }
+    // massRes.forEach(element => {
+    //   res = res + element;
+    // });
+    if (this.direction) {
+      massRes.forEach(element => {
+        res = res + element;
+      });
+    } else {
+      massRes.reverse().forEach(element => {
+        res = res + element;
+      });
+    }
+  return res; 
   }
+
+  decrypt(encryptedMessage, key) {
+    if (!key || !encryptedMessage) {
+      throw new Error('Incorrect arguments!');
+    }
+    
+    let newKey = '';
+    let numOfKey = Math.floor(encryptedMessage.length / key.length);
+    for (let i = 0; i < numOfKey; i++) {
+      newKey = newKey + key;
+    }
+    for (let j = 0; j < (encryptedMessage.length % key.length); j++) {
+      newKey = newKey + key[j]
+    }
+    console.log(newKey);  
+    let res = '';
+    let alphabeth = ['A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    let rows = alphabeth.length;
+    let massMessage = encryptedMessage.toUpperCase().split('');
+    let massRes = [];
+    let count = 0;
+    console.log(massMessage);
+    for (let x = 0; x < massMessage.length; x++) {
+      if (alphabeth.includes(massMessage[x])) {
+        console.log(massMessage[x]);
+        let indA = alphabeth.indexOf(massMessage[x].toUpperCase());
+        console.log(indA);
+        let indB = alphabeth.indexOf(newKey[count].toUpperCase());
+        console.log(indB);
+        massRes.push(alphabeth[(26 + indA - indB) % 26]);
+        count ++;
+      } else {
+        massRes.push(massMessage[x]);
+      }
+    }
+    // massRes.forEach(element => {
+    //   res = res + element;
+    // });
+    if (this.direction) {
+      massRes.forEach(element => {
+        res = res + element;
+      });
+    } else {
+      massRes.reverse().forEach(element => {
+        res = res + element;
+      });
+    }
+   return res; 
+   }
 }
 
 module.exports = {
